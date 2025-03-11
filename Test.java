b02661acc90ed48474c9b8224fd486d90592dfa8
anurag <anurag.patil@nimapinfotech.com>
2025-03-11 15:18:35 +0530

    Done

--- Test.java
+++ Test.java
@@ -1,2 +1,20 @@
 public class Test {
+    public static void main(String[] args) {
+        Dog dog = new Dog();      // Creating a Dog object
+        Animal animal = dog;      // Upcasting: Dog -> Animal
+        animal.speak();           // Calling speak() method
+    }
+}
+
+class Animal {
+    public void speak(){
+        System.out.println("....");  // Animal's version of speak
+    }
+}
+
+class Dog extends Animal{
+    @Override
+    public void speak(){
+        System.out.println("Dog Barks");  // Dog's version of speak
+    }
 }
